df89e10b82b603540c6e96ddc0c297168808e5a6
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-05-01 22:19:31 +0000

    Deck cards generation

--- src/main/java/org/rozz/Deck.java
+++ src/main/java/org/rozz/Deck.java
@@ -1,4 +1,24 @@
 package org.rozz;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Deck {
+    private ArrayList<BlackjackCard> cardDeck;
+
+    private ArrayList<BlackjackCard> shuffledCards;
+
+    private Date creationDate;
+
+    public Deck() {
+        this.creationDate = new Date();
+        this.cardDeck = new ArrayList<>();
+        for (CardInfo cardInfo: CardInfo.values()) {
+            String name = cardInfo.getName();
+            int value = cardInfo.getValue();
+            for (Suit suit: Suit.values()) {
+                cardDeck.add(new BlackjackCard(name, suit, value));
+            }
+        }
+    }
 }
