343ff5789dfba489aa9d1a027988f941308a1c47
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-05-01 22:22:37 +0000

    chnaged name to CardInfo

---
+++ src/main/java/org/rozz/CardInfo.java
@@ -0,0 +1,27 @@
+package org.rozz;
+
+public enum CardInfo {
+
+    One("Ace",1), Two("two",2),
+    Three("three",3), Four("four", 4),
+    Five("five", 5), Six("six",6),
+    Seven("seven", 7), Eight("eight",8),
+    Nine("nine",9), Ten("ten",10),
+    Jack("jack", 10), Queen("queen", 10),
+    King("king", 10);
+
+    private String name;
+    private int value;
+    CardInfo(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
