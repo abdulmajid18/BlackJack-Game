3709582d5c966b1702afc46492a481f48dccc611
amnuhu <abdul-majid.nuhu@turntabl.io>
2023-05-03 07:01:32 +0000

    making random test

--- src/main/java/org/rozz/Main.java
+++ src/main/java/org/rozz/Main.java
@@ -2,6 +2,14 @@ package org.rozz;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        Deck deck = new Deck();
+
+        System.out.println(deck.getCardDeck());
+
+        Shoe shoe = new Shoe(2);
+        System.out.println(shoe.getCards().size());
     }
 }
